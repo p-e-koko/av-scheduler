0213b3b8a4a4888479c43936a9a18a03d114adad
p-e-koko <panneikoko1221@gmail.com>
2025-12-14 19:45:05 +0700

    changing default MAIL FROM ADDRESS

--- backend/config/mail.php
+++ backend/config/mail.php
@@ -111,7 +111,7 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
+        'address' => env('MAIL_FROM_ADDRESS', 'no-reply@pann.khazifire.com'),
         'name' => env('MAIL_FROM_NAME', 'Example'),
     ],
 
